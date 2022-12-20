04452e1402d36463ea7e45be3a18dd915aef8198
Jsheen98 <jsheendev@gmail.com>
2022-12-20 15:36:37 -0700

    Added the README.md file, as well as implemented final fixes to JS

---
+++ README.md
@@ -0,0 +1,61 @@
+# Challenge 3: Random Password Generator
+
+
+## Description
+
+With this project, the goal was to create a password generator that would that would give a user several customization options. The user is able to choose character types including:
+
+* Lowercase letters
+* Uppercase letters
+* Numbers
+* Special characters (or symbols)
+
+The user is also able to choose the amount of characters, as long as it's more than 8 and less than 128. It also only allows numbers in this field (no letters, symbols, etc.). When the user clicks 'Generate Password' several pop-ups appear in sequence to have the user select their preferences for password generation.
+
+## User Story
+
+* AS AN employee
+* I WANT to randomly generate a password that meets certain criteria
+* SO THAT I can create a strong password that provides greater security
+
+
+## Acceptance Criteria
+
+* GIVEN I need a new, secure password
+* WHEN I click the button to generate a password
+* THEN I am presented with a series of prompts for password criteria
+* WHEN prompted for password criteria
+* THEN I select which criteria to include in the password
+* WHEN prompted for the length of the password
+* THEN I choose a length of at least 8 characters and no more than 128 characters
+* WHEN asked for character types to include in the password
+* THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
+* WHEN I answer each prompt
+* THEN my input should be validated and at least one character type should be selected
+* WHEN all prompts are answered
+* THEN a password is generated that matches the selected criteria
+* WHEN the password is generated
+* THEN the password is either displayed in an alert or written the page
+
+
+## Installation
+
+N/A
+
+
+## Deployed Application
+
+The deployed application can be found at this link:
+https://jsheen98.github.io/coding-bootcamp-challenge-3/
+
+
+## Assets
+
+The following image demonstrates the website's appearance:
+
+![Password Generator Image](./assets/images/password-generator-screenshot.png)
+
+
+## License
+
+Please refer to the LICENSE in the repo
--- script.js
+++ script.js
@@ -4,89 +4,100 @@ var lowercase = 'abcdefghijklmnopqrstuvwxyz';
 var uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 var numeric = '1234567890';
 var specialChars = '!@#$%^&*()?<>';
+var length = 0;
+var chars = '';
+var retVal = '';
+
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-  
+
 
   function generatePassword() {
-    var retVal = '';
-    var length = 0;
-    numberOfCharactersCheck()
-    chars = characterTypeSelectionCheck();
-
-  
-  // function that puts confirms on the page to have the user select what character types they want   
-    function characterTypeSelectionCheck() {
-      var chars = '';
-      
-      if (confirm("Would you like your password to contain lowercase letters?")) {
-        chars += lowercase
-      }
-      else {chars}
-      
-      if (confirm("Would you like your password to contain uppercase letters?")) {
-        chars += uppercase
-      }
-      else {chars}
-      
-      if (confirm("Would you like your password to contain numbers?")) {
-        chars += numeric
-      }
-      else {chars}
-      
-      if (confirm("Would you like your password to contain special characters?")) {
-        chars += specialChars
-      }
-      else {chars} 
-
-      // if user hasn't selected any type of character, it sends them back through the confirms until they select at least one type
-      if (chars == '') {
-        alert('NOTE: User must select AT LEAST 1 character type.')
-        characterTypeSelectionCheck()
-      }
-      else {
-        return chars
-      }
-    }
     
-
-
-    // Function that allows user to select number of characters in their password
-    function numberOfCharactersCheck() {
-      var numberOfCharactersPrompt = +prompt("How many characters would you like your password to have?");
-        
-        if (numberOfCharactersPrompt < 8) {
-          (alert("Password must at least 8 characters"))
-          numberOfCharactersCheck()
-        }
-        else if (numberOfCharactersPrompt > 128) {
-          alert("Password can't contain more than 128 characters")
-          numberOfCharactersCheck()
-        }
-        // If user selects an input that is not a number, it resets the prompt
-        else if (isNaN(numberOfCharactersPrompt)) {
-          alert("User must input a number")
-          numberOfCharactersCheck()
-        }
-        else {
-          length += numberOfCharactersPrompt
-          return length
-        }
-    }
-
-    for (var i = 0, n = chars.length; i < length; i++) {
-      retVal += chars.charAt(Math.floor(Math.random() * n))
+    numberOfCharactersCheck();
+    characterTypeSelectionCheck();
+    
+    
+    for (var i = 0; i < length; i++) {
+      retVal += chars.charAt(Math.floor(Math.random() * chars.length));
     }
     return retVal
-
   }
   
   passwordText.value = password;
-
+  clearField()
 }
+
+
+// function that puts confirms on the page to have the user select what character types they want   
+function characterTypeSelectionCheck() {
+
+
+  if (confirm("Would you like your password to contain lowercase letters?")) {
+    chars += lowercase
+  }
+  else { chars }
+
+  if (confirm("Would you like your password to contain uppercase letters?")) {
+    chars += uppercase
+  }
+  else { chars }
+
+  if (confirm("Would you like your password to contain numbers?")) {
+    chars += numeric
+  }
+  else { chars }
+
+  if (confirm("Would you like your password to contain special characters?")) {
+    chars += specialChars
+  }
+  else { chars }
+  // if user hasn't selected any type of character, it sends them back through the confirms until they select at least one type
+  if (chars == '') {
+    alert('NOTE: User must select AT LEAST 1 character type.')
+    characterTypeSelectionCheck()
+  }
+  else {
+    return chars
+  }
+}
+
+
+// Function that allows user to select number of characters in their password
+function numberOfCharactersCheck() {
+  var numberOfCharactersPrompt = prompt("How many characters would you like your password to have?");
+
+  if (numberOfCharactersPrompt < 8) {
+    (alert("Password must at least 8 characters"))
+    numberOfCharactersCheck()
+  }
+  else if (numberOfCharactersPrompt > 128) {
+    alert("Password can't contain more than 128 characters")
+    numberOfCharactersCheck()
+  }
+  // If user selects an input that is not a number, it resets the prompt
+  else if (isNaN(numberOfCharactersPrompt)) {
+    alert("User must input a number")
+    numberOfCharactersCheck()
+  }
+  else {
+    length += parseInt(numberOfCharactersPrompt)
+    return length
+  }
+}
+
+
+function clearField() {
+  length = 0;
+  chars = '';
+  retVal = 0;
+  chars.length = 0;
+  i = 0;
+}
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
